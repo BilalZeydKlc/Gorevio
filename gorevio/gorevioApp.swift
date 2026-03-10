c62e03e6e9257d10476cc90fa454310a95cee6a4
Bilal Zeyd Kılıç <kilicbilalzeyd@gmail.com>
2026-03-10 14:05:44 +0300

    Initial Commit

---
+++ gorevio/gorevioApp.swift
@@ -0,0 +1,17 @@
+//
+//  gorevioApp.swift
+//  gorevio
+//
+//  Created by Bilal Zeyd Kılıç on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct gorevioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
